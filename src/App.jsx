6ff0e86d4c5811c794ca6f76c07178e6926dc3ab
mangesh <mangeshlemte.a23@gmail.com>
2025-01-01 18:59:18 +0530

    style: fix z index issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home, Notfound,Notification} from "./pages/index";
-import { Login, SignUp,Dashboard } from "./components/index";
-import Profile from "./components/Profile"
-import { ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import { Home, Notfound, Notification } from "./pages/index";
+import { Login, SignUp, Dashboard } from "./components/index";
+import Profile from "./components/Profile";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AnimatedCursor from "react-animated-cursor";
 function App() {
-   return (
+  return (
     <>
       <BrowserRouter>
         <Routes>
@@ -17,25 +17,32 @@ function App() {
           <Route path="*" element={<Notfound />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/" element={<Dashboard />} />
-              <Route path="/" index element={<Home />} />
+          <Route path="/" index element={<Home />} />
           <Route path="/notification" element={<Notification />} />
         </Routes>
       </BrowserRouter>
-      <ToastContainer position="top-right" autoClose={1200} />
       <AnimatedCursor
-  innerSize={8}
-  outerSize={35}
-  innerScale={1}
-  outerScale={2}
-  outerAlpha={0}
-  hasBlendMode={true}
-  innerStyle={{
-    backgroundColor: 'var(--cursor-color)'
-  }}
-  outerStyle={{
-    border: '3px solid var(--cursor-color)'
-  }}
-/>
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        zIndex={9999}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+          pointerEvents: "none",
+        }}
+        outerStyle={{
+          border: "3px solid var(--cursor-color)",
+          pointerEvents: "none",
+        }}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={1200}
+        style={{ zIndex: 555 }}
+      />
     </>
   );
 }
