d83ecc5f2ebc5dd22ce615785dcac0d699181136
mangesh <mangeshlemte.a23@gmail.com>
2025-03-09 20:58:14 +0530

    Refactor Modal component to include a form for user input with validation

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 import {
-  Input,
   Button,
   Dialog,
-   Typography,
+  Typography,
   DialogBody,
   DialogHeader,
-  DialogFooter,
 } from "@material-tailwind/react";
 import { X } from "lucide-react";
+import { useForm } from "react-hook-form";
 const Modal = () => {
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => setOpen(!open);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+  };
   return (
     <>
       <Button
@@ -29,8 +37,7 @@ const Modal = () => {
         size="lg"
         open={open}
         handler={handleOpen}
-        className="p-4 max-w-[550px] text-center mx-auto backdrop-blur-none"
-     
+        className="p-2 max-w-[550px] text-center mx-auto"
       >
         <DialogHeader className="relative m-0 block">
           <Typography variant="h4" color="blue-gray" className="capitalize">
@@ -42,44 +49,133 @@ const Modal = () => {
           <X
             className="h-7 w-7 stroke-4 !absolute right-3.5 top-3.5 cursor-pointer"
             size="xl"
-           
             onClick={handleOpen}
           />
         </DialogHeader>
-        <DialogBody className="space-y-4 pb-6">
-          <div>
-            <Typography
-              variant="small"
-              color="blue-gray"
-              className="mb-2 text-left font-medium"
-            >
-              Enter Total Monthly Investment
-            </Typography>
-            <form action="" method="post">
-              
+        {/* form */}
+        <DialogBody className=" pb-3">
+          <div className="max-w-md  mx-auto bg-white ">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
+              {/* Name Field */}
+              <div>
+                <label className="block text-gray-700 text-left">
+                  Name Of Nominee
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name Of Nominee"
+                  {...register("name", { required: "Name is required" })}
+                  className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                />
+                {errors.name && (
+                  <p className="text-red-500 text-left">
+                    {errors.name.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Email Field */}
+              <div>
+                <label className="block text-gray-700 capitalize text-left">
+                  Add Email for alerts (Email sent to Nominee)
+                </label>
+                <input
+                  type="email"
+                  placeholder="Add Email for alerts (Email sent to Nominee)"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "Invalid email address",
+                    },
+                  })}
+                  className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                />
+                {errors.email && (
+                  <p className="text-red-500 text-left">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+
+              <div>
+                <label className="block text-gray-700 text-left">
+                  Enter Monthly investment in Mutual fund
+                </label>
+                <input
+                  type="number"
+                  placeholder="Monthly investment in Mutual fund"
+                  {...register("mutual_fund", { required: "Value required" })}
+                  className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                />
+                {errors.mutual_fund && (
+                  <p className="text-red-500 text-left">
+                    {errors.mutual_fund.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label className="block text-gray-700 text-left">
+                  Enter Monthly investment in FD
+                </label>
+                <input
+                  type="number"
+                  placeholder="Monthly investment in FD"
+                  {...register("fd_value", { required: "Value required" })}
+                  className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                />
+                {errors.fd_value && (
+                  <p className="text-red-500 text-left">
+                    {errors.fd_value.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label className="block text-gray-700 text-left">
+                  Enter Monthly investment in PPF{" "}
+                </label>
+                <input
+                  type="number"
+                  placeholder="Monthly investment in PPF"
+                  {...register("ppf_value", { required: "Value is required" })}
+                  className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                />
+                {errors.ppf_value && (
+                  <p className="text-red-500 text-left">
+                    {errors.ppf_value.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <label className="block text-gray-700 text-left">
+                  Enter Monthly investment in Stocks
+                </label>
+                <input
+                  type="number"
+                  placeholder="Monthly investment in Stocks"
+                  {...register("stocks_value", {
+                    required: "Value is required",
+                  })}
+                  className="w-full px-4 py-2 border-2 border-black rounded-lg"
+                />
+                {errors.stocks_value && (
+                  <p className="text-red-500 text-left">
+                    {errors.stocks_value.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Submit Button */}
+              <button
+                type="submit"
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+              >
+                Submit
+              </button>
             </form>
-            <Input
-              color="gray"
-              size="lg"
-              placeholder="Enter Total Monthly Investment"
-              name="name"
-              type="number"
-              className="placeholder:opacity-100 focus:!border-t-gray-900"
-              containerProps={{
-                className: "!min-w-full",
-              }}
-              labelProps={{
-                className: "hidden",
-              }}
-              required
-            />
           </div>
         </DialogBody>
-        <DialogFooter>
-          <Button className="ml-auto bg-blue-500" onClick={handleOpen}>
-            Add Data
-          </Button>
-        </DialogFooter>
       </Dialog>
     </>
   );
