04acd3560053bcfc7ae13a2c8639404c55466f22
mangesh <mangeshlemte.a23@gmail.com>
2025-03-09 18:16:43 +0530

    add new avatar

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,9 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import React, { useEffect } from "react";
-import {Navbar} from "../index"
+import { Navbar, Sidebar } from "../index";
 import { useUser } from "../../contextAPI";
+import male from "../../assets/character/male.png";
 const Profile = () => {
-  const {user} = useUser()
+  const { user } = useUser();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,27 +16,26 @@ const Profile = () => {
   return (
     <div>
       <Navbar user={user} />
-      {user && (
-        <div className="max-w-screen-xl mx-auto">
-          <div className="p-4 flex justify-center items-center flex-wrap flex-col text-center">
-            <div className="rounded-full">
-              <img
-                src="https://placehold.co/120"
-                className="rounded-full"
-                alt="profile image"
-              />
-            </div>
-            <h3 className="text-xl font-semibold text-gray-700 mt-7">
-              Email: {user.email}
-            </h3>
-            <div>
+      <div className="flex flex-1">
+        <Sidebar />
+        {user && (
+          <div className="max-w-screen-xl mx-auto">
+            <div className="p-4 flex justify-center items-center flex-wrap flex-col text-center">
+              <div className="rounded-full w-[200px] border-2 border-blue-500 ">
+                <img src={male} className="rounded-full" alt="profile image" />
+              </div>
               <h3 className="text-xl font-semibold text-gray-700 mt-7">
-                Name: {user.displayName}
+                Email: {user.email}
               </h3>
+              <div>
+                <h3 className="text-xl font-semibold text-gray-700 mt-7">
+                  Name: {user.displayName}
+                </h3>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
