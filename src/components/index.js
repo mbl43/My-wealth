d5890722a74a9e6823a45199ceb17f800baea678
mangesh <mangeshlemte.a23@gmail.com>
2025-03-09 18:14:55 +0530

    Added path

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,9 @@ import SignUp from "./Signup/SignUp"
 import Dashboard from "./Dashboard/Dashboard"
 import Sidebar from "./Sidebar/Sidebar"
 import Profile from "./profile/Profile"
-import Sip from "./Sip/Sip"
-export{Footer,Navbar,Hero,Login,SignUp,Dashboard,Sidebar,Profile,Sip}
+import Sip from "./Calc/Sip"
+import Dialog from "./Dialog/Dialog";
+import Modal from "../components/Modal/Modal"
+import StockAvg from "../components/Calc/StockAvg";
+import LossRecovery from "../components/Calc/LossRecovery"
+export{Footer,Navbar,Hero,Login,SignUp,Dashboard,Sidebar,Profile,Sip,Dialog,Modal,StockAvg,LossRecovery}
